bcb21e4a06143c36c3b67298b86b7f2aaa94f870
Kyle Williams <15004488+elykkyle@users.noreply.github.com>
2023-03-07 11:18:52 -0600

    create alert component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Alert from './components/layout/Alert';
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
           <div className='flex flex-col justify-between h-screen'>
             <Navbar />
             <main className='container mx-auto px-3 pb-12'>
+              <Alert />
               <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
